191b42d2c3ef53b6c7a65ae5869cb546febc0dc8
Nick <70416083+ztivnick@users.noreply.github.com>
2022-07-04 20:28:00 -0400

    Added basic timer and card switcher

--- MMM-StudySets.js
+++ MMM-StudySets.js
@@ -1,20 +1,57 @@
 Module.register("MMM-StudySets", {
   defaults: {
     category: "Education",
-    text: "Welcome to study sets!",
+    card: "Welcome to study sets!",
+    // Update progress every second
+    timerInterval: 1000,
+    progress: 0,
   },
 
   getStyles() {
     return [this.file("MMM-StudySets.css")];
   },
 
+  notificationReceived(notification, payload, sender) {
+    if (notification === 'MODULE_DOM_CREATED') {
+      setInterval(() => {
+        const currentProgress = this.defaults.progress;
+        if (currentProgress >= 10) {
+          // Start back at 10% so cycle only takes 10 seconds
+          this.defaults.progress = 1;
+          this.getNewStudySetCard();
+        }
+        else {
+          this.defaults.progress = this.defaults.progress + 1;
+        }
+        this.updateDom();
+      }, this.config.timerInterval);
+    }
+  },
+
   getDom: function () {
     var wrapper = document.createElement("div");
-    wrapper.innerHTML = this.config.text;
+    var card = document.createElement("div");
+    card.innerHTML = this.defaults.card;
+    wrapper.innerHTML = this.cardTimer(this.defaults.progress)
+    wrapper.appendChild(card);
+
     return wrapper;
   },
 
-  start: function () {
-    Log.log(this.name + " started");
+  cardTimer: function (progress) {
+    let timer = "";
+    for (let i = 1; i <= 10; i += 1) {
+      timer = (i > progress) ? timer + "◯" : timer + "⬤";
+    }
+    console.log("Updated timer at " + progress + "0%");
+    return timer;
   },
+
+  getNewStudySetCard: function (card) {
+    // Pick a random new card or use specified card
+    // TODO: Use specified card
+    // TODO: Get card from file
+    this.defaults.card = (card == null) ? Math.floor(Math.random() * 10) : "";
+  }
+
 });
